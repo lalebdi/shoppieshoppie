4c73ba44f19267c206dc2dff4090d5ef27f3a5d8
Loai Alebdi <loaialebdi@Loais-MBP.fios-router.home>
2020-07-11 13:07:49 -0400

    first step

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Shoppingcart from './components/Shoppingcart.jsx'
 
-function App() {
+class  App extends Component  {
+  render(){
   return (
     <div className="App">
       <Shoppingcart />
     </div>
   );
+}
 }
 
 export default App;
