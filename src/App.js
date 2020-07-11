f91a807a0a56283219505f99d5de85fba5f1ad80
Loai Alebdi <loaialebdi@Loais-MBP.fios-router.home>
2020-07-11 16:19:58 -0400

    Finally in the right direction

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
-import Shoppingcart from './components/Shoppingcart.jsx'
+import Products from './components/Products'
+
 
 class  App extends Component  {
+  constructor(props){
+    super(props)
+    this.state = { products:[] , filteredProducts: [] , }
+  }
+  componentWillMount(){
+    fetch("http://localhost:8001/products").then(res => res.json())
+    .then(data => this.setState({
+      products:data,
+      filteredProducts:data
+    }));
+  }
   render(){
   return (
-    <div className="App">
-      <Shoppingcart />
+    <div className="container">
+      <h1>Leah's Kitchen Shopping cart</h1>
+      <br/>
+      <div className="row">
+        <div className="col-md-8">
+          <Products products={this.state.filteredProducts} handleAddToCart = {this.handleAddToCart} />
+        </div>
+        <div className="col-md-4">
+
+        </div>
+      </div>
     </div>
   );
 }
